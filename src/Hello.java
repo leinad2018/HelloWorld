c23a696eeb9fc3fc7d5d8691940d909e3dd0d74b
leinad2018 <leinad2018@gmail.com>
2018-01-29 11:20:57 -0600

    added city, state, zip

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("Daniel");
 		System.out.println("1234 Hill Court");
+		System.out.println("Brookfield, WI, 44444");
 	}
 
 }
